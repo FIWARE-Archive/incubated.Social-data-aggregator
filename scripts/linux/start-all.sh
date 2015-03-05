2ba3b0da2ad814a8f40a0cf5fbb59ad00fa5928e
dinoolivo <dino.olivo@gmail.com>
2015-03-05 17:45:51 +0100

    start-all: added possibility to not load spark env

--- scripts/linux/start-all.sh
+++ scripts/linux/start-all.sh
@@ -1,6 +1,42 @@
+function display_usage_and_exit {
+  echo "start-all.sh - "
+  echo ""
+  echo "usage:"
+  echo "./start-tw-connector.sh --sda-home <sda_home> --no-start-spark-env"
+  echo ""
+  echo "--sda-home -> The path of social-data-aggregator folder. Optional. Provide from command line if SDA_HOME is not setted in your environment"
+  echo "--no-start-spark-env -> avoid to launch spark in standalone mode before submit sda realtime modules"
+  echo ""
+  exit 1
+}
+
+
 echo "Called SDA start-all.."
 
+SCRIPT_DIRNAME=$(dirname "$0")
+cd $SCRIPT_DIRNAME
 source confs.cfg
+#finire parte tenendo conto della variabile NO_ENV
+# Parse arguments
+while (( "$#" )); do
+  case $1 in
+    --sda-home)
+       SDA_HOME=$2
+	   shift
+      ;;
+	--no-start-spark-env)
+	   NO_ENV=1
+	   shift
+	  ;;
+    --help)
+      display_usage_and_exit
+      ;;
+    *)
+      break
+      ;;
+  esac
+  shift
+done
 #####
 if [ "x$SDA_HOME" == "x" ]
 then
@@ -16,11 +52,13 @@ then
 fi
 export SDA_HOME
 
-SCRIPT_DIRNAME=$(dirname "$0")
-cd $SCRIPT_DIRNAME
 #####
-echo "loading env.."
-#source start-spark-env.sh
+if [ "x$NO_ENV" == "x" ]
+then
+	echo "loading spark env.."
+	source start-spark-env.sh
+fi
+
 export MASTER=spark://$SPARK_MASTER_IP:$SPARK_MASTER_PORT
 
 while read row 
